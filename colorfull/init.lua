89c64a09f76a962034e0f9f15786feaeb30572f8
mindys3993 <mindys3993@gmail.com>
2026-02-03 13:10:33 +0300

    Update init.lua

--- colorfull/init.lua
+++ colorfull/init.lua
@@ -1,21 +1,18 @@
+-- Simple node registration for 4 palette layers
 for i = 0, 3 do
     minetest.register_node("colorfull:pixel_" .. i, {
         description = "Cinema Pixel Layer " .. i,
         tiles = {"white_block.png"},
         paramtype2 = "color",
         palette = "pal_" .. i .. ".png", 
-        -- Настройка групп: 
-        -- oddly_breakable_by_hand = 3 (ломается рукой быстро)
-        -- snappy = 3 (быстро ломается мечом/рукой)
-        -- dig_immediate = 3 (моментальная поломка, как у факелов или цветов)
         groups = {
             oddly_breakable_by_hand = 3, 
             snappy = 3, 
-            dig_immediate = 3,
+            dig_immediate = 3, -- Instant break
             not_in_creative_inventory = 1
         },
         sunlight_propagates = true,
-        pointable = true, -- Теперь на блок можно навестись и ударить
+        pointable = true,
         is_ground_content = false,
     })
 end
